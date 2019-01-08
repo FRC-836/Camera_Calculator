ec9793f6d86f99c325239c3591f2fae70de18cd9
Cheeto836 <mprussell836@gmail.com>
2019-01-07 19:46:16 -0500

    basic working version loading an empty GUI

---
+++ inc/MainWindow.h
@@ -0,0 +1,36 @@
+#ifndef CAMERA_CALCULATOR_MAIN_WINDOW_H
+#define CAMERA_CALCULATOR_MAIN_WINDOW_H
+
+#include <memory>
+
+#include <QMainWindow>
+
+#include "ui_MainWindow.h"
+#include "OutputManager.h"
+#include "CmdOptions.h"
+
+class MainWindow : public QMainWindow
+{
+  private:
+    //member variables
+    std::unique_ptr<Ui_MainWindow> m_ui;
+
+    //private functions
+    void makeConnections();
+
+  public:
+    //constructors
+    MainWindow();
+    ~MainWindow();
+
+    //public functions
+
+  signals:
+    //signals
+
+  public slots:
+    //public slots
+
+};
+
+#endif
